2140961f6b40152e03ed5e00333c47e2d0abf03c
Marcos Vinicius <gbarbosa@fatorseguradora.com.br>
2021-11-09 12:29:50 -0300

    Boleto Bancario Orientado a Objetos

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Emissão de boletos</title>
+</head>
+<body>
+    <a href="boleto_bancoob.php">Boleto Bancoob</a>
+    <a href="boleto_banespa.php">Boleto Banespa</a>
+    <a href="boleto_banestes.php">Boleto Banestes</a>
+    <a href="boleto_bb.php">Boleto Banco do Brasil</a>
+    <a href="boleto_besc.php">Boleto Besc</a>
+    <a href="boleto_bradesco.php">Boleto Bradesco</a>
+    <a href="boleto_cef_sigcb.php">Boleto CEF Sigcb</a>
+    <a href="boleto_cef_sinco.php">Boleto CEF Sinco</a>
+    <a href="boleto_cef.php">Boleto Caixa Economica Federal</a>
+    <a href="boleto_hsbc.php">Boleto Hsbc</a>
+    <a href="boleto_itau.php">Boleto Itaú</a>
+    <a href="boleto_nossacaixa.php">Boleto Nossa Caixa</a>
+    <a href="boleto_real.php">Boleto Real</a>
+    <a href="boleto_santander_banespa.php">Boleto Santander</a>
+    <a href="boleto_sicredi.php">Boleto Sicredi</a>
+    <a href="boleto_sofisa.php">Boleto Sofisa</a>
+    <a href="boleto_sudameris.php">Boleto Sudameris</a>
+    <a href="boleto_unibanco.php">Boleto Unibanco</a>
+</body>
+</html>
